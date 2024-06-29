93df8b2135965f8b9cd5ed3ddb488c02191de965
Wang <rwan370@aucklanduni.ac.nz>
2024-06-29 12:20:56 +0800

    run the repositories with python

--- PyGitHub.py
+++ PyGitHub.py
@@ -4,7 +4,7 @@ from github import Github
 from github import Auth
 
 # using an access token
-auth = Auth.Token("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")
+auth = Auth.Token("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")
 
 # First create a Github instance:
 
@@ -12,7 +12,7 @@ auth = Auth.Token("github_pat_11BG7LTTI0gnpIe5M2kM5N_DvE4nBsrTrdHx4B73JXjCsRCLgk
 g = Github(auth=auth)
 
 # Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", auth=auth)
+g = Github(base_url="https://api.github.com", auth=auth)
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
@@ -20,3 +20,4 @@ for repo in g.get_user().get_repos():
 
 # To close connections after use
 g.close()
+
---
+++ RunSample.py
@@ -0,0 +1,13 @@
+from PyGitHub import g
+
+# for repo in g.get_user().get_repos():
+#     print(repo.name)
+#     repo.edit(has_wiki=False)
+#     # to see all the available attributes and methods
+#     print(dir(repo))
+#
+# g.close()
+
+repositories = g.search_repositories(query='language:python')
+for repo in repositories:
+    print(repo)
