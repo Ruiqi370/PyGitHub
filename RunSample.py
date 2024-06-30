206a01362495cac8c0dd1a44f8f9499d463c619d
Wang <rwan370@aucklanduni.ac.nz>
2024-06-30 11:33:07 +0800

    test for max repository limit

--- RunSample.py
+++ RunSample.py
@@ -7,11 +7,29 @@ from PyGitHub import g
 #     print(dir(repo))
 #
 # g.close()
+# import time
+# from PyGitHub import g
+#
+# repositories = g.search_repositories(query='language:python')
+# for repo in repositories:
+#     print(repo)
+#     time.sleep(2)  # delay for 2 seconds
+#
+#
+# repositories = g.search_repositories(query='good-first-issues:>3')
+# for repo in repositories:
+#    print(repo)
+#    time.sleep(2)  # delay for 2 seconds
+import time
+from PyGitHub import g
+
+cnt=0
+max_repos=100
 
 repositories = g.search_repositories(query='language:python')
 for repo in repositories:
+    if cnt >= max_repos:
+       break
     print(repo)
-
-repositories = g.search_repositories(query='good-first-issues:>3')
-for repo in repositories:
-   print(repo)
+    cnt+=1
+    time.sleep(1)  # delay for 2 seconds
